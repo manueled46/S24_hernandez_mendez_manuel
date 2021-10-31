b54fa1a9b3cbdb70cc92a1486f6a6d5ffb33c874
manueled46 <92635889+manueled46@users.noreply.github.com>
2021-10-31 16:49:29 -0600

    Add files via upload

---
+++ T1E1_pos_neg.py
@@ -0,0 +1,10 @@
+#Programador: Manuel Hernandez
+if __name__ == '__main__':
+   print("Bienvenido al programa")
+   a=int(input("Ingrese un numero"))
+   if a<0:
+      print("Es un numero negativo")
+   elif a==0:
+      print("El numero es cero")
+   else:
+      print("El numero es positivo")
