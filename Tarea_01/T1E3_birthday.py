0b4479c7d330d1d525dadead49e3ae9aea2e7246
manueled46 <92635889+manueled46@users.noreply.github.com>
2021-10-31 18:33:48 -0600

    Add files via upload

---
+++ Tarea_01/T1E3_birthday.py
@@ -0,0 +1,7 @@
+#Programador: Manuel Hernandez
+if __name__ == '__main__':
+    a = int(input("Ingrese su año de nacimiento"))
+    b = int((input("Ingrese el mes")))
+    c = int((input("Dia")))
+    print(f"Year: {a} - Month {b} - Day {c}")
+    print("Year: {} - Month {} - Day {}".format(a,b,c))
