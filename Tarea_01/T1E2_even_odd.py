c02f349cdae7c7aadebbfd38d1c4d2d1a86cd962
manueled46 <92635889+manueled46@users.noreply.github.com>
2021-10-31 18:12:02 -0600

    Add files via upload

---
+++ Tarea_01/T1E2_even_odd.py
@@ -0,0 +1,7 @@
+#Programador: Manuel Hernandez
+if __name__ == '__main__':
+    a=int(input("Ingrese un numero"))
+    if a%2==0:
+        print("El numero es par")
+    else:
+        print("Es impar")
