e4fa109a65ac43bade357ea19dabf59d94500372
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-11-18 15:50:42 -0600

    Update T1E1_pos_neg.py

--- Tarea_01/T1E1_pos_neg.py
+++ Tarea_01/T1E1_pos_neg.py
@@ -1,10 +1,32 @@
-#Programador: Manuel Hernandez
+"""
+Date:       2021-11-05
+Author:     Manuel Yahir Hernández Méndez
+File:       T1E1_pos_neg.py
+Brief:      Ingresar un número y validar si es un número positivo,
+            negativo o cero.
+Score:      40
+Version:    0.1.1
+Fixes:      - PEP8 recomienda añadir un espacio en blanco después del
+                carácter # de los comentarios
+
+            - PEP8 recomienda dejar una línea en blanco al final del
+                código
+
+            - PEP8 recomienda que la sangría sea un múltiplo de 4
+                espacio (o una tabulación como les sugerí)
+
+            - PEP8 recomienda añadir espacios en blanco alrededor de
+                los operadores
+"""
+
+#Programador: Manuel Hernandez          # PEP8
 if __name__ == '__main__':
-   print("Bienvenido al programa")
-   a=int(input("Ingrese un numero"))
-   if a<0:
-      print("Es un numero negativo")
-   elif a==0:
-      print("El numero es cero")
-   else:
-      print("El numero es positivo")
+   print("Bienvenido al programa")      # PEP8
+   a=int(input("Ingrese un numero"))    # PEP8
+   if a<0:                              # PEP8 PEP8
+      print("Es un numero negativo")    # PEP8
+   elif a==0:                           # PEP8 PEP8
+      print("El numero es cero")        # PEP8
+   else:                                # PEP8
+      print("El numero es positivo")    # PEP8 PEP8
+      
