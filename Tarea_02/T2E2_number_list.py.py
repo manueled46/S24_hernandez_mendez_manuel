8798b06be826fa7aa96ac78b730611f331541bf2
manueled46 <92635889+manueled46@users.noreply.github.com>
2021-11-22 17:43:33 -0600

    Update T2E2_number_list.py.py

--- Tarea_02/T2E2_number_list.py.py
+++ Tarea_02/T2E2_number_list.py.py
@@ -13,28 +13,34 @@ if __name__ == '__main__':
     fin = 0;
     #Imprimir los elementos y el tipo de lista
     #1
+    
     print("---------------")
     print(lista)
     print(type(lista))
     #Convertir los elementos a int
     #2
+    
     print("---------------")
     lista = list(map(int,lista))
     print(lista)
     #Imprimir un elemento por el usuario
     #3
+    
     print("---------------")
     print("A continuacion digite el rango de la lista que desea ver")
-    inicio=int(input("Digite el inicio de la lista"))
-    fin=int(input("Digite el final de la lista"))
+    inicio = int(input("Digite el inicio de la lista"))
+    fin = int(input("Digite el final de la lista"))
     print(lista[inicio:fin])
     #Ordenar la lista
     #4
+    
     print("---------------")
     lista.sort()
     print(lista)
     #Ordenar la lista al reves
     #5
+    
     print("---------------")
     lista.sort(reverse= True)
     print(lista)
+    
