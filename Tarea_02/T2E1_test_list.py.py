60a0ecbbdfdb5db508b57b516c6285769c05184b
manueled46 <92635889+manueled46@users.noreply.github.com>
2021-11-22 17:44:33 -0600

    Update T2E1_test_list.py.py

--- Tarea_02/T2E1_test_list.py.py
+++ Tarea_02/T2E1_test_list.py.py
@@ -8,25 +8,31 @@ Version: 1
 Fixes:
 '''
 if __name__ == '__main__':
+    
     #Creacion de la lista
     lista = ["Calculo","Fisica","Algebra","Humanidades","Qumica"]
     print(lista)
     #Metodo append
     #1
+    
     lista.append("Tutoria")
     print(lista)
     #Metodo inset
     #2
+    
     lista.insert(0,"Bioquimica")
     print(lista)
     #Metodo pop
     #3
+    
     lista.pop(0)
     print(lista)
     #Ordenar mi lista de A-Z
     #4
+    
     lista.sort()
     print(lista)
     #Ordenar mi lista de Z-A
-    lista.sort(reverse=True)
+    
+    lista.sort(reverse = True)
     print(lista)
