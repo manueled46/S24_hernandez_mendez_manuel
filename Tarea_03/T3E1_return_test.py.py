ffc8add2dd6bebb6251de1f371af9cbe1ddc7751
manueled46 <92635889+manueled46@users.noreply.github.com>
2021-11-22 20:06:30 -0600

    Add files via upload

---
+++ Tarea_03/T3E1_return_test.py.py
@@ -0,0 +1,22 @@
+"""
+Date: 2021-11/22
+Authors: Manuel Yahir Hernandez Mendez
+File:     T3E1_return_test.py
+Brief: Pedimos un numero y regresamos un string
+Score:
+Version: 1
+Fixes:
+"""
+
+
+def conversion(a):
+    a = str(a)
+    print(a)
+
+
+if __name__ == '__main__':
+
+    numero = 0
+
+    numero = int(input("Digite un numero"))
+    conversion(numero)
