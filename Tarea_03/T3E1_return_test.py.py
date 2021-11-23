2f2f366337bd7bc0616b36eacef63e3f4d366cfa
manueled46 <92635889+manueled46@users.noreply.github.com>
2021-11-22 21:43:40 -0600

    Update T3E1_return_test.py.py

--- Tarea_03/T3E1_return_test.py.py
+++ Tarea_03/T3E1_return_test.py.py
@@ -1,6 +1,7 @@
 """
 Date: 2021-11/22
 Authors: Manuel Yahir Hernandez Mendez
+         Mauricio Isaias Bolaños Vázquez
 File:     T3E1_return_test.py
 Brief: Pedimos un numero y regresamos un string
 Score:
