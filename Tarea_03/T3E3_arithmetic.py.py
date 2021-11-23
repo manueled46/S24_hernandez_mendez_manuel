b60ced95b6b576122ccab5a9b6c3cdd082cf73f8
manueled46 <92635889+manueled46@users.noreply.github.com>
2021-11-22 21:45:25 -0600

    Update T3E3_arithmetic.py.py

--- Tarea_03/T3E3_arithmetic.py.py
+++ Tarea_03/T3E3_arithmetic.py.py
@@ -1,6 +1,7 @@
 """
 Date: 2021-11/22
 Authors: Manuel Yahir Hernandez Mendez
+         Mauricio Isaias Bolaños Vázquez
 File:     T3E3_arithmetic.py
 Brief:   Operaciones de numeros flotantes
 Score:
