e29fa862b2576ea9c6e9c17c63cc924737088aac
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2021-12-07 15:57:39 -0600

    Update T1E2_even_odd.py

--- Tarea_01/T1E2_even_odd.py
+++ Tarea_01/T1E2_even_odd.py
@@ -1,10 +1,17 @@
-#Programador: Manuel Hernandez
-
+"""
+Date:       2021-10-31
+Author:     Manuel Yahir Hernández Méndez
+File:       T1E2_even_odd.py
+Brief:      Ingresar un número y validar si es par o impar
+Score:      100
+Version:    1.1.1
+Fixes:      -
+"""
 
 if __name__ == '__main__':
     a = int(input("Ingrese un numero"))
-    if a%2 == 0:
+    # Seria bueno validar e indicar cuando el número es cero
+    if a % 2 == 0:
         print("El numero es par")
     else:
         print("Es impar")
-        
