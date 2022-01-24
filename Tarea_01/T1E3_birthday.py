62e2916570e8a3602e14da02c90f8efdb79a4e08
Félix Armenta A <62727466+FelixH316@users.noreply.github.com>
2022-01-23 23:16:36 -0600

    Update T1E3_birthday.py

--- Tarea_01/T1E3_birthday.py
+++ Tarea_01/T1E3_birthday.py
@@ -1,5 +1,18 @@
-#Programador: Manuel Hernandez
-
+"""
+Date:     2021-11-22
+Authors:  Manuel Yahir Hernandez Mendez
+File:     T1E3_birthday.py
+Brief:    Este programa solicita año, mes y día de cumpleaños 
+          e implime el string 'Year:xxxx - Month:xx - Day: xx'
+          con sus respectivos resultados
+Score:    80
+Version:  1.1.1
+Fixes:    - PEP8 recomienda añadir un espacio en blanco después del
+                carácter de coma ','
+          
+          - Faltaron los dos puntos ':' en el formato de impresion
+                solicitado
+"""
 
 if __name__ == '__main__':
     a = int(input("Ingrese su año de nacimiento"))
@@ -7,4 +20,3 @@ if __name__ == '__main__':
     c = int((input("Dia")))
     print(f"Year: {a} - Month {b} - Day {c}")
     print("Year: {} - Month {} - Day {}".format(a,b,c))
-    
